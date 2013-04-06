d018a59d993998a26944f4a9b44f53132921248f
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-04-06 14:17:37 +0300

    get config from environment

--- conf.py
+++ conf.py
@@ -4,8 +4,8 @@ import os
 class Config(object):
     DEBUG = True
     TESTING = False
-    SECRET_KEY = "asntoheusnatoehu"
+    SECRET_KEY = "asntoheusnatoehu" # TODO: make that a proper secret ^^ (possible to generate at startup?)
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_APP_ID = '483391628381431'
-    FBAPI_APP_SECRET = 'ffc510a57f1731e3129e0c85b239c854'
+    FBAPI_APP_ID = os.environ.get('FBAPI_APP_ID')
+    FBAPI_APP_SECRET = os.environ.get('FBAPI_APP_SECRET')
     FBAPI_SCOPE = ['user_likes', 'user_photos', 'user_photo_video_tags', 'read_mailbox']
